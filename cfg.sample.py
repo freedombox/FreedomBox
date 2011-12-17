69f3375a4f02986e11c4e86b8c0b153a1a5683ce
James Vasile <james@hackervisions.org>
2011-12-16 23:43:30 -0500

    start adding base_href

--- cfg.sample.py
+++ cfg.sample.py
@@ -3,6 +3,7 @@ import os
 
 file_root = os.path.dirname(os.path.realpath(__file__))
 data_dir = os.path.join(file_root, "data")
+base_href=""
 store_file = os.path.join(data_dir, "store.sqlite3")
 user_db = os.path.join(data_dir, "users")
 status_log_file = os.path.join(data_dir, "status.log")
