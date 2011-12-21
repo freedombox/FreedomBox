c22e3695092d27fa7bd84c0a9733185ef479ea08
James Vasile <james@hackervisions.org>
2011-12-20 21:25:20 -0500

    move basehref

--- cfg.sample.py
+++ cfg.sample.py
@@ -3,7 +3,6 @@ import os
 
 file_root = os.path.dirname(os.path.realpath(__file__))
 data_dir = os.path.join(file_root, "data")
-base_href=""
 store_file = os.path.join(data_dir, "store.sqlite3")
 user_db = os.path.join(data_dir, "users")
 status_log_file = os.path.join(data_dir, "status.log")
@@ -18,4 +17,5 @@ port = 8000
 ## Do not edit below this line ##
 html_root = None
 main_menu = Menu()
+base_href = ""
 
