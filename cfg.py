8d7f2b4dbdc64a28f6864e607b1a18e1f2018b11
James Vasile <james@jamesvasile.com>
2013-02-06 15:56:09 -0500

    move to ini file and config parser

---
+++ cfg.py
@@ -0,0 +1,38 @@
+from menu import Menu
+import os
+
+from ConfigParser import SafeConfigParser
+parser = SafeConfigParser(
+    defaults={
+        'root':os.path.dirname(os.path.realpath(__file__)),
+        'product_name':"",
+        'box_name':"",
+        'file_root':"",
+        'data_dir':"",
+        'store_file':"",
+        'user_db':"",
+        'status_log_file':"",
+        'access_log_file':"",
+        'users_dir':"",
+        'host':"127.0.0.1",
+        'port':""
+        })
+parser.read('plinth.config')
+
+product_name = parser.get('Name', 'product_name')
+box_name = parser.get('Name', 'box_name')
+root = parser.get('Path', 'root')
+file_root = parser.get('Path', 'file_root')
+data_dir = parser.get('Path', 'data_dir')
+store_file = parser.get('Path', 'store_file')
+user_db = parser.get('Path', 'user_db')
+status_log_file = parser.get('Path', 'status_log_file')
+access_log_file = parser.get('Path', 'access_log_file')
+users_dir = parser.get('Path', 'users_dir')
+host = parser.get('Network', 'host')
+port = int(parser.get('Network', 'port'))
+
+html_root = None
+main_menu = Menu()
+base_href = ""
+
