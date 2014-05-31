22760ceb500fb2bd25a66124b881722dde465d2c
Sunil Mohan Adapa <sunil@medhas.org>
2014-05-31 20:21:28 +0530

    Remove Photos stub

--- modules/installed/apps/apps.py
+++ modules/installed/apps/apps.py
@@ -12,15 +12,8 @@ class Apps(PagePlugin):
         self.register_page("apps")
         self.menu = cfg.main_menu.add_item("Apps", "icon-download-alt", "/apps", 80)
         self.menu.add_item("Chat", "icon-comment", "/../jwchat", 30)
-        self.menu.add_item("Photo Gallery", "icon-picture", "/apps/photos", 35)
 
     @cherrypy.expose
     def index(self):
         return util.render_template(template='apps',
                                     title=_('User Applications'))
-
-    @cherrypy.expose
-    @require()
-    def photos(self):
-        return util.render_template(template='photos',
-                                    title=_('Photo Gallery'))
