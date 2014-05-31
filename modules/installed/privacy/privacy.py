de250e5e63e4b0a36d06f8187644f91157265218
Sunil Mohan Adapa <sunil@medhas.org>
2014-05-31 23:18:23 +0530

    Remove Privacy stubs

--- modules/installed/privacy/privacy.py
+++ modules/installed/privacy/privacy.py
@@ -12,17 +12,8 @@ class Privacy(PagePlugin):
         PagePlugin.__init__(self, *args, **kwargs)
         self.register_page("privacy")
         self.menu = cfg.main_menu.add_item("Privacy", "icon-eye-open", "/privacy", 12)
-        self.menu.add_item("General Config", "icon-asterisk", "/privacy/config", 10)
-        self.menu.add_item("Ad Blocking", "icon-ban-circle", "/privacy/adblock", 20)
-        self.menu.add_item("HTTPS Everywhere", "icon-lock", "/privacy/https_everywhere", 30)
 
     @cherrypy.expose
     def index(self):
-        #raise cherrypy.InternalRedirect('/privacy/config')
-        return self.config()
-
-    @cherrypy.expose
-    @require()
-    def config(self):
         return util.render_template(template='privacy_config',
                                     title=_('Privacy Control Panel'))
