314ae1fc03255c3b46dea01c516c50cd5f468e59
fonfon <blubb@fonfon.at>
2014-09-15 13:32:12 +0200

    added tor service

---
+++ plinth/modules/tor/vault.py
@@ -0,0 +1,19 @@
+from plinth import actions
+from plinth.modules import vault
+
+
+def enable():
+    return actions.superuser_run('tor', ['start'])
+
+
+def disable():
+    return actions.superuser_run('tor', ['stop'])
+
+
+def is_running():
+    return actions.superuser_run("tor", ["is-running"]).strip() == "yes"
+
+
+vault.register_service(name='tor', is_enabled=is_running,
+                       enable=enable, disable=disable,
+                       description='Tor anonymization service')
