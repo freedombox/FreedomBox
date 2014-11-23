86ce7cdf25abddb60c2b6e13e320ced35e9d4b77
Sunil Mohan Adapa <sunil@medhas.org>
2014-11-23 14:15:45 +0530

    Use globe glyphicon for pagekite

--- plinth/modules/pagekite/pagekite.py
+++ plinth/modules/pagekite/pagekite.py
@@ -40,7 +40,7 @@ LOGGER = logging.getLogger(__name__)
 def init():
     """Intialize the PageKite module"""
     menu = cfg.main_menu.get('apps:index')
-    menu.add_urlname(_('Public Visibility (PageKite)'), 'glyphicon-flag',
+    menu.add_urlname(_('Public Visibility (PageKite)'), 'glyphicon-globe',
                      'pagekite:index', 50)
 
 
