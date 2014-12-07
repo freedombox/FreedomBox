a0cd525b96a3718d401962d550c5f72d8cdf1527
Sunil Mohan Adapa <sunil@medhas.org>
2014-12-07 11:33:10 +0530

    Add explicit license from test coverage script
    The license on the blog post is GPLv3+.  The derived work can't be of
    any other license but GPLv3+.

--- plinth/tests/coverage/test_coverage.py
+++ plinth/tests/coverage/test_coverage.py
@@ -1,10 +1,29 @@
 #!/usr/bin/python3
 # -*- mode: python; mode: auto-fill; fill-column: 80 -*-
+#
+# This file is part of Plinth.
+#
+# Dervied from code sample at:
+# http://jeetworks.org/adding-test-code-coverage-analysis-to-a-python-projects-setup-command/
+#
+# Copyright 2009 Jeet Sukumaran and Mark T. Holder.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program. If not, see <http://www.gnu.org/licenses/>.
+#
 
-"""Support for integration of code test coverage analysis with setuptools.
-
-Derived from 'Adding Test Code Coverage Analysis to a Python Project’s Setup Command'
-<http://jeetworks.org/adding-test-code-coverage-analysis-to-a-python-projects-setup-command/>
+"""
+Support for integration of code test coverage analysis with setuptools.
 """
 
 import glob
