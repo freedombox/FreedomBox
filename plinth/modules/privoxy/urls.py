6a462cecc8b83d2afb9c96730e99e476576c1f88
Sunil Mohan Adapa <sunil@medhas.org>
2016-02-28 15:11:48 +0530

    privoxy: Use common view for configuration

--- plinth/modules/privoxy/urls.py
+++ plinth/modules/privoxy/urls.py
@@ -21,9 +21,10 @@ URLs for the Privoxy module.
 
 from django.conf.urls import url
 
-from . import views
+from plinth.views import ConfigurationView
 
 
 urlpatterns = [
-    url(r'^apps/privoxy/$', views.index, name='index'),
+    url(r'^apps/privoxy/$', ConfigurationView.as_view(module_name='privoxy'),
+        name='index'),
 ]
