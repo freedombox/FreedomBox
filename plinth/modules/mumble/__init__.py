75d35b3d6046e2ae680c9e0bc3aca529470e4bd0
Sunil Mohan Adapa <sunil@medhas.org>
2016-02-28 15:06:11 +0530

    mumble: Use common view for configuration

--- plinth/modules/mumble/__init__.py
+++ plinth/modules/mumble/__init__.py
@@ -21,6 +21,7 @@ Plinth module to configure Mumble server
 
 from django.utils.translation import ugettext_lazy as _
 
+from plinth import actions
 from plinth import action_utils
 from plinth import cfg
 from plinth import service as service_module
@@ -60,6 +61,12 @@ def setup(helper, old_version=None):
     helper.call('post', service.notify_enabled, None, True)
 
 
+def get_status():
+    """Get the current settings from server."""
+    return {'enabled': is_enabled(),
+            'is_running': is_running()}
+
+
 def is_enabled():
     """Return whether the module is enabled."""
     return action_utils.service_is_enabled('mumble-server')
@@ -68,6 +75,13 @@ def is_enabled():
 def is_running():
     """Return whether the service is running."""
     return action_utils.service_is_running('mumble-server')
+
+
+def enable(should_enable):
+    """Enable/disable the module."""
+    sub_command = 'enable' if should_enable else 'disable'
+    actions.superuser_run('mumble', [sub_command])
+    service.notify_enabled(None, should_enable)
 
 
 def diagnose():
