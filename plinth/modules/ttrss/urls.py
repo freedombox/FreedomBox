d60c2c074e07ab48825c3fef650c2fcdae9122dc
pk13055 <pkrockstar7@gmail.com>
2016-11-17 13:31:24 +0530

    Status block enabled.

--- plinth/modules/ttrss/urls.py
+++ plinth/modules/ttrss/urls.py
@@ -30,6 +30,6 @@ urlpatterns = [
             service_id=ttrss.managed_services[0],
             diagnostics_module_name="ttrss",
             description=ttrss.description,
-            show_status_block=False
+            show_status_block=True
         ), name='index'),
 ]
