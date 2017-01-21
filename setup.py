6f455d6afc8369aa1a6719568302d88921c7e224
Marko Mackic <markomackic@gmail.com>
2017-01-21 19:00:16 +0100

    Remove dir_util copy_tree

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ Plinth setup file
 
 from distutils import log
 from distutils.command.build import build
-from distutils.dir_util import remove_tree,copy_tree
+from distutils.dir_util import remove_tree
 from distutils.command.clean import clean
 from distutils.command.install_data import install_data
 from distutils.core import Command
