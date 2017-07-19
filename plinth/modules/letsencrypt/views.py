3ea5868ec1c8c9669534413400916f3e9626b69b
James Valleroy <jvalleroy@mailbox.org>
2017-07-18 21:26:49 -0400

    pylint and spelling fixes

--- plinth/modules/letsencrypt/views.py
+++ plinth/modules/letsencrypt/views.py
@@ -19,14 +19,15 @@
 Plinth module for using Let's Encrypt.
 """
 
+import json
+import logging
+
 from django.contrib import messages
 from django.shortcuts import redirect
 from django.template.response import TemplateResponse
 from django.urls import reverse_lazy
 from django.utils.translation import ugettext as _
 from django.views.decorators.http import require_POST
-import json
-import logging
 
 from plinth import actions
 from plinth.errors import ActionError
@@ -138,10 +139,12 @@ def get_status():
     status = actions.superuser_run('letsencrypt', ['get-status'])
     status = json.loads(status)
     curr_dom = config.get_domainname()
-    current_domain = {'name': curr_dom,
-                      'has_cert': curr_dom in status['domains'] and
-                      status['domains'][curr_dom]['certificate_available'],
-                      'manage_hooks_enabled': _hooks_manage_enabled()}
+    current_domain = {
+        'name': curr_dom,
+        'has_cert': (curr_dom in status['domains'] and
+                     status['domains'][curr_dom]['certificate_available']),
+        'manage_hooks_enabled': _hooks_manage_enabled()
+    }
     status['current_domain'] = current_domain
 
     for domain_type, domains in names.domains.items():
@@ -156,10 +159,10 @@ def get_status():
 
 
 def _hooks_manage_enabled():
-        """Return status of hook management for current domain."""
-        try:
-            output = actions.superuser_run('letsencrypt',
-                                           ['manage_hooks', 'status'])
-        except ActionError:
-            return False
-        return output.strip() == 'enabled'
+    """Return status of hook management for current domain."""
+    try:
+        output = actions.superuser_run('letsencrypt',
+                                       ['manage_hooks', 'status'])
+    except ActionError:
+        return False
+    return output.strip() == 'enabled'
