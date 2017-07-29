3e38a3e3f1383d925cb716a7aa81a81b5e862f22
James Valleroy <jvalleroy@mailbox.org>
2017-07-29 10:50:06 -0400

    diaspora: fix test file header

--- plinth/modules/diaspora/tests/test_apache_config.py
+++ plinth/modules/diaspora/tests/test_apache_config.py
@@ -1,4 +1,5 @@
-#!/usr/bin/pytho part of Plinth.
+#
+# This file is part of Plinth.
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as
