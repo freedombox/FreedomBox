246ca33e187a8918ab8a528ff7234b4669dd0958
Federico Ceratto <federico.ceratto@gmail.com>
2017-10-26 21:25:13 +0100

    Disable test when sudo is not available

--- plinth/modules/letsencrypt/tests/test_domain_name_changes.py
+++ plinth/modules/letsencrypt/tests/test_domain_name_changes.py
@@ -18,11 +18,15 @@
 Tests for letsencrypt module.
 """
 
+import os.path
 import unittest
 
 from .. import on_domain_added, on_domain_removed
 
+sudo_available = os.path.isfile("/usr/bin/sudo")
 
+
+@unittest.skipUnless(sudo_available, 'Requires sudo')
 class TestDomainNameChanges(unittest.TestCase):
     """Test for automatically obtaining and revoking Let's Encrypt certs"""
 
