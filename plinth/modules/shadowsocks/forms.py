2ee82e4b5a72131b084e48bab69d50d4ee529c56
Sunil Mohan Adapa <sunil@medhas.org>
2017-11-29 17:44:39 +0530

    shadowsocks: Add more ciphers
    Signed-off-by: Sunil Mohan Adapa <sunil@medhas.org>

--- plinth/modules/shadowsocks/forms.py
+++ plinth/modules/shadowsocks/forms.py
@@ -23,8 +23,25 @@ from django import forms
 from django.utils.translation import ugettext_lazy as _
 
 from plinth.forms import ServiceForm
+from plinth.utils import format_lazy
 
-METHODS = ['chacha20-ietf-poly1305', 'aes-256-gcm']
+METHODS = [
+    ('chacha20-ietf-poly1305',
+     format_lazy('chacha20-ietf-poly1305 ({})', _('Recommended'))),
+    ('aes-256-gcm', format_lazy('aes-256-gcm ({})', _('Recommended'))),
+    ('aes-192-gcm', 'aes-192-gcm'),
+    ('aes-128-gcm', 'aes-128-gcm'),
+    ('aes-128-ctr', 'aes-128-ctr'),
+    ('aes-192-ctr', 'aes-192-ctr'),
+    ('aes-256-ctr', 'aes-256-ctr'),
+    ('aes-128-cfb', 'aes-128-cfb'),
+    ('aes-192-cfb', 'aes-192-cfb'),
+    ('aes-256-cfb', 'aes-256-cfb'),
+    ('camellia-128-cfb', 'camellia-128-cfb'),
+    ('camellia-192-cfb', 'camellia-192-cfb'),
+    ('camellia-256-cfb', 'camellia-256-cfb'),
+    ('chacha20-ietf', 'chacha20-ietf')
+]
 
 
 class TrimmedCharField(forms.CharField):
@@ -57,5 +74,5 @@ class ShadowsocksForm(ServiceForm):
 
     method = forms.ChoiceField(
         label=_('Method'),
-        choices=[(method, method) for method in METHODS],
+        choices=METHODS,
         help_text=_('Encryption method. Must match setting on server.'))
