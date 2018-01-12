0609f6a107025083159a33eafb9c015a825317ae
Sunil Mohan Adapa <sunil@medhas.org>
2018-01-12 13:56:37 +0530

    groups: Don't duplicate checking of group existence
    Signed-off-by: Sunil Mohan Adapa <sunil@medhas.org>

--- plinth/modules/users/forms.py
+++ plinth/modules/users/forms.py
@@ -109,7 +109,6 @@ class CreateUserForm(ValidNewUsernameCheckMixin, UserCreationForm):
 
             for group in self.cleaned_data['groups']:
                 try:
-                    users.create_group(group)
                     actions.superuser_run('users', [
                         'add-user-to-group',
                         user.get_username(), group
@@ -206,7 +205,6 @@ class UserUpdateForm(ValidNewUsernameCheckMixin, forms.ModelForm):
             for new_group in new_groups:
                 if new_group not in old_groups:
                     try:
-                        users.create_group(new_group)
                         actions.superuser_run('users', [
                             'add-user-to-group',
                             user.get_username(), new_group
