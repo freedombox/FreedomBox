b4f75ded793b3df6dc40c3605ea9bb1b64c57150
Sunil Mohan Adapa <sunil@medhas.org>
2018-02-13 12:00:55 +0530

    locale: YAFP and isort formatting
    Signed-off-by: Sunil Mohan Adapa <sunil@medhas.org>
    Reviewed-by: James Valleroy <jvalleroy@mailbox.org>

--- plinth/forms.py
+++ plinth/forms.py
@@ -14,27 +14,26 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
-
 """
 Common forms for use by modules.
 """
 
+import os
+
 from django import forms
 from django.conf import settings
 from django.utils import translation
-from django.utils.translation import ugettext_lazy as _, get_language_info
+from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import get_language_info
 
 import plinth
 from plinth import utils
 
-import os
-
 
 class ServiceForm(forms.Form):
     """Generic configuration form for a service."""
-    is_enabled = forms.BooleanField(
-        label=_('Enable application'),
-        required=False)
+    is_enabled = forms.BooleanField(label=_('Enable application'),
+                                    required=False)
 
 
 class DomainSelectionForm(forms.Form):
@@ -49,10 +48,8 @@ class DomainSelectionForm(forms.Form):
     domain_name = forms.ChoiceField(
         label=_('Select a domain name to be used with this application'),
         help_text=_(
-            'Warning! The application may not work properly if domain name is changed later.'
-        ),
-        choices=[]
-    )
+            'Warning! The application may not work properly if domain name is '
+            'changed later.'), choices=[])
 
 
 class LanguageSelectionForm(forms.Form):
@@ -66,7 +63,9 @@ class LanguageSelectionForm(forms.Form):
             plinth_dir = os.path.dirname(plinth.__file__)
             if language_code == 'en' or os.path.exists(
                     os.path.join(plinth_dir, 'locale', locale_code)):
-                supported_languages.append((language_code, get_language_info(language_code)['name_local']))
+                supported_languages.append(
+                    (language_code,
+                     get_language_info(language_code)['name_local']))
 
         self.fields['language'].choices = supported_languages
 
