6536e86b1581b7649d77420b8d82eb18e34eb08a
James Valleroy <jvalleroy@mailbox.org>
2019-02-10 18:13:42 -0500

    radicale: Fix server URLs in client info
    Signed-off-by: James Valleroy <jvalleroy@mailbox.org>
    Reviewed-by: Sunil Mohan Adapa <sunil@medhas.org>

--- plinth/modules/radicale/manifest.py
+++ plinth/modules/radicale/manifest.py
@@ -25,7 +25,7 @@ clients = validate([{
         _('DAVx5'),
     'usage':
         _('Enter the URL of the Radicale server (e.g. '
-          'http://localhost:5232) and your user name. DAVx5 will '
+          'https://<your.freedombox.address>) and your user name. DAVx5 will '
           'show all existing calendars and address books and you can '
           'create new.'),
     'platforms': [{
@@ -77,7 +77,7 @@ clients = validate([{
     'usage':
         _('In Evolution add a new calendar and address book '
           'respectively with WebDAV. Enter the URL of the Radicale '
-          'server (e.g. http://localhost:5232) and your user name. '
+          'server (e.g. https://<your.freedombox.address>) and your user name. '
           'Clicking on the search button will list the existing '
           'calendars and address books.'),
     'platforms': [{
