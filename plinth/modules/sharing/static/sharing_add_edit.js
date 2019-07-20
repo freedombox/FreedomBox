9fc8c3604cab3f9031a1597d2967c21983f8dcce
Joseph Nuthalapati <njoseph@thoughtworks.com>
2019-07-20 23:37:11 +0530

    sharing: Add JavaScript to hide user groups for public shares
    - User groups will only be shown when the shares are not public.
    - Checking the checkbox for public shares hides the user groups.
    - Unchecking the checkbox for public shares shows the user groups.
    
    Signed-off-by: Joseph Nuthalapati <njoseph@thoughtworks.com>
    Reviewed-by: James Valleroy <jvalleroy@mailbox.org>

---
+++ plinth/modules/sharing/static/sharing_add_edit.js
@@ -0,0 +1,36 @@
+/**
+ * @licstart The following is the entire license notice for the JavaScript
+ * code in this page.
+ *
+ * This file is part of FreedomBox.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @licend The above is the entire license notice for the JavaScript code
+ * in this page.
+ */
+
+var sharing_groups = $('#id_sharing-groups').parent().parent();
+
+if ($("#id_sharing-is_public").prop('checked')) {
+    sharing_groups.hide();
+}
+
+$("#id_sharing-is_public").change(function() {
+    if (this.checked) {
+        sharing_groups.hide();
+    } else {
+        sharing_groups.show();
+    }
+});
