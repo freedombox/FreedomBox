6a6983f39ef8b17521843a83551968d40c395ff0
Sunil Mohan Adapa <sunil@medhas.org>
2019-10-23 11:22:53 -0700

    storage: Use AppView and cleanup custom code
    - Follow common code so that extending becomes easier.
    
    Signed-off-by: Sunil Mohan Adapa <sunil@medhas.org>
    Reviewed-by: James Valleroy <jvalleroy@mailbox.org>

--- plinth/modules/storage/urls.py
+++ plinth/modules/storage/urls.py
@@ -23,7 +23,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^sys/storage/$', views.index, name='index'),
+    url(r'^sys/storage/$', views.StorageAppView.as_view(), name='index'),
     url(r'^sys/storage/expand$', views.expand, name='expand'),
     url(r'^sys/storage/eject/(?P<device_path>[A-Za-z0-9%_.\-~]+)/$',
         views.eject, name='eject')
