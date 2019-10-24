ddb95752a87027c49f4368aa147a09dc344a2835
Nektarios Katakis <iam@nektarioskatakis.xyz>
2019-10-24 16:52:03 +0100

    ssh: Option for disabling password authentication
    Closes: #572
    
    [sunil: Reload instead of restart, fix i18n strings]
    [sunil: Load augeas minimally, minor cosmetic fixes]
    Signed-off-by: Sunil Mohan Adapa <sunil@medhas.org>
    Reviewed-by: Sunil Mohan Adapa <sunil@medhas.org>
    Signed-off-by: Sunil Mohan Adapa <sunil@medhas.org>

--- plinth/modules/ssh/views.py
+++ plinth/modules/ssh/views.py
@@ -17,9 +17,15 @@
 """
 Views for the SSH module
 """
+from django.contrib import messages
+from django.utils.translation import ugettext_lazy as _
 
+from plinth import action_utils, actions
 from plinth.modules import ssh
 from plinth.views import AppView
+
+from . import is_password_authentication_disabled
+from .forms import SSHServerForm
 
 
 class SshAppView(AppView):
@@ -28,9 +34,43 @@ class SshAppView(AppView):
     description = ssh.description
     port_forwarding_info = ssh.port_forwarding_info
     template_name = 'ssh.html'
+    form_class = SSHServerForm
 
     def get_context_data(self, *args, **kwargs):
         context = super().get_context_data(**kwargs)
         context['host_keys'] = ssh.get_host_keys()
 
         return context
+
+    def get_initial(self):
+        """Initial form value"""
+        initial = super().get_initial()
+        initial.update({
+            'password_auth_disabled': is_password_authentication_disabled(),
+        })
+
+        return initial
+
+    def form_valid(self, form):
+        """Apply changes from the form"""
+        old_config = self.get_initial()
+        new_config = form.cleaned_data
+
+        def is_field_changed(field):
+            return old_config[field] != new_config[field]
+
+        passwd_auth_changed = is_field_changed('password_auth_disabled')
+        if passwd_auth_changed:
+            if new_config['password_auth_disabled']:
+                passwd_auth = 'no'
+                message = _('SSH authentication with password disabled.')
+            else:
+                passwd_auth = 'yes'
+                message = _('SSH authentication with password enabled.')
+
+            actions.superuser_run(
+                'ssh', ['set-password-config', '--value', passwd_auth])
+            actions.superuser_run('service', ['reload', 'ssh'])
+            messages.success(self.request, message)
+
+        return super().form_valid(form)
