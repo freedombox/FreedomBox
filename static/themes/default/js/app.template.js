1a65c88881670ad6b364320bb923cb52b36d53b9
Alice Kile <buoyantair@protonmail.com>
2019-11-11 17:34:07 +0530

    app: Use single form for app toggle and configuration
    Reviewed-by: Joseph Nuthalapati <njoseph@riseup.net>

--- static/themes/default/js/app.template.js
+++ static/themes/default/js/app.template.js
@@ -1,18 +1,18 @@
-const appToggleForm = document.querySelector('#app-toggle')
-const appToggleContainer = appToggleForm.parentElement
+const appForm = document.querySelector('#app-form')
+const appToggleContainer = document.querySelector('#app-toggle-container')
+const appToggleButton = document.querySelector('#app-toggle-button')
 const appToggleInput = document.querySelector('#app-toggle-input')
 
 const onSubmit = (e) => {
   e.preventDefault
   appToggleInput.checked = !appToggleInput.checked
-  appToggleForm.submit()
+  appForm.submit()
 }
-
-appToggleForm.addEventListener('submit', onSubmit)
-
+appToggleButton.addEventListener('click', onSubmit)
 
 /**
  * if javascript is enabled, this script will run and show the toggle button
  */
 
+appToggleInput.parentElement.style.display = 'none'
 appToggleContainer.style.display = 'flex';
