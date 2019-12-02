9d20875b0356cac34de8c338bfe4c4256cf72085
Veiko Aasa <veiko17@disroot.org>
2019-12-02 18:09:33 +0300

    samba: Fix javascript constant redeclaration error
    Related to #1715
    
    Signed-off-by: Veiko Aasa <veiko17@disroot.org>
    Reviewed-by: Joseph Nuthalapati <njoseph@riseup.net>

--- plinth/modules/samba/static/samba.js
+++ plinth/modules/samba/static/samba.js
@@ -21,10 +21,13 @@
  * in this page.
  */
 
-const share_checkbox = $(".shareform > input[type='checkbox']");
 
-share_checkbox.change(function(event) {
+$(document).on('turbolinks:load', function() {
+  const share_checkbox = $(".shareform > input[type='checkbox']");
+
+  share_checkbox.change(function(event) {
     this.disabled=true;
     this.style.cursor='wait';
     this.form.submit();
+  });
 });
