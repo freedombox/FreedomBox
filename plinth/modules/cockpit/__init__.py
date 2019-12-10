656988cc546e569f794365b2c9781defac967082
Nektarios Katakis <iam@nektarioskatakis.xyz>
2019-12-10 12:03:19 +0000

    cockpit: extend apps description with access info
    Reviewed-by: James Valleroy <jvalleroy@mailbox.org>

--- plinth/modules/cockpit/__init__.py
+++ plinth/modules/cockpit/__init__.py
@@ -56,7 +56,11 @@ description = [
     format_lazy(
         _('It can be accessed by <a href="{users_url}">any user</a> on '
           '{box_name} belonging to the admin group.'),
-        box_name=_(cfg.box_name), users_url=reverse_lazy('users:index'))
+        box_name=_(cfg.box_name), users_url=reverse_lazy('users:index')),
+    format_lazy(
+        _('Cockpit requires that you access it through a domain name. '
+          'It will not work when accessed using an IP address as part'
+          ' of the URL.')),
 ]
 
 manual_page = 'Cockpit'
