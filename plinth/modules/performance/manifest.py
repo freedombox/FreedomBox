f74ca9044a40acdbee96130edfd4e7886e1809a5
Sunil Mohan Adapa <sunil@medhas.org>
2020-05-12 11:19:20 -0700

    performance: Launch the Cockpit graphs directly if possible
    Signed-off-by: Sunil Mohan Adapa <sunil@medhas.org>
    Reviewed-by: James Valleroy <jvalleroy@mailbox.org>

--- plinth/modules/performance/manifest.py
+++ plinth/modules/performance/manifest.py
@@ -4,12 +4,13 @@ FreedomBox app for System Monitoring (cockpit-pcp) in ‘System’.
 """
 
 from django.utils.translation import ugettext_lazy as _
+
 from plinth.clients import validate
 
 clients = validate([{
     'name': _('Cockpit'),
     'platforms': [{
         'type': 'web',
-        'url': '/_cockpit/'
+        'url': '/_cockpit/system/graphs'
     }]
 }])
