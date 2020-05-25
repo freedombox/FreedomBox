bb3b32fd694b9559469daea9e8b02bb7cf34dfd6
Sunil Mohan Adapa <sunil@medhas.org>
2020-05-25 01:08:43 -0700

    samba: Add clients information
    Closes: #1814.
    
    Tests performed:
    
    - Connect with all the mentioned clients to a Samba folder on a FreedomBox.
    
    - Visit all the links in the clients information table.
    
    Signed-off-by: Sunil Mohan Adapa <sunil@medhas.org>
    Reviewed-by: Veiko Aasa <veiko17@disroot.org>

--- plinth/modules/samba/manifest.py
+++ plinth/modules/samba/manifest.py
@@ -3,12 +3,79 @@
 Application manifest for Samba.
 """
 
-from plinth.clients import validate
+from django.utils.translation import ugettext_lazy as _
+
+from plinth.clients import store_url, validate
 from plinth.modules.backups.api import validate as validate_backup
 
 SHARES_CONF_BACKUP_FILE = '/var/lib/plinth/backups-data/samba-shares-dump.conf'
 
-clients = validate([])
+clients = validate([{
+    'name':
+        _('Android Samba Client'),
+    'platforms': [{
+        'type':
+            'store',
+        'os':
+            'android',
+        'store_name':
+            'f-droid',
+        'url':
+            store_url('f-droid', 'com.google.android.sambadocumentsprovider')
+    }]
+}, {
+    'name':
+        _('Ghost Commander - Samba plugin'),
+    'platforms': [{
+        'type': 'store',
+        'os': 'android',
+        'store_name': 'f-droid',
+        'url': store_url('f-droid', 'com.ghostsq.commander.samba')
+    }, {
+        'type': 'store',
+        'os': 'android',
+        'store_name': 'google-play',
+        'url': store_url('google-play', 'com.ghostsq.commander.smb')
+    }]
+}, {
+    'name':
+        _('VLC media player'),
+    'platforms': [{
+        'type': 'store',
+        'os': 'android',
+        'store_name': 'f-droid',
+        'url': store_url('f-droid', 'org.videolan.vlc')
+    }, {
+        'type': 'store',
+        'os': 'android',
+        'store_name': 'google-play',
+        'url': store_url('google-play', 'org.videolan.vlc')
+    }]
+}, {
+    'name':
+        _('GNOME Files'),
+    'platforms': [{
+        'type': 'package',
+        'format': 'deb',
+        'name': 'nautilus',
+    }, {
+        'type': 'download',
+        'os': 'gnu-linux',
+        'url': 'https://wiki.gnome.org/Apps/Files'
+    }]
+}, {
+    'name':
+        _('Dolphin'),
+    'platforms': [{
+        'type': 'package',
+        'format': 'deb',
+        'name': 'dolphin',
+    }, {
+        'type': 'download',
+        'os': 'gnu-linux',
+        'url': 'https://kde.org/applications/system/org.kde.dolphin'
+    }]
+}])
 
 backup = validate_backup({
     'data': {
