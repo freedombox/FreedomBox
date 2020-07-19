c7bf1bb46f5d8a1b455cf7f40980b1e0b5bd69cc
James Valleroy <jvalleroy@mailbox.org>
2020-07-19 10:26:01 -0400

    tests: functional: Strip trailing / from FREEDOMBOX_URL
    Some web app tests combine the configured URL with expected paths. Avoid failing
    these tests when there is a trailing slash at the end of the URL.
    
    Signed-off-by: James Valleroy <jvalleroy@mailbox.org>
    Reviewed-by: Sunil Mohan Adapa <sunil@medhas.org>

--- plinth/tests/functional/__init__.py
+++ plinth/tests/functional/__init__.py
@@ -21,7 +21,7 @@ config = configparser.ConfigParser()
 config.read(pathlib.Path(__file__).with_name('config.ini'))
 
 config['DEFAULT']['url'] = os.environ.get('FREEDOMBOX_URL',
-                                          config['DEFAULT']['url'])
+                                          config['DEFAULT']['url']).rstrip('/')
 config['DEFAULT']['samba_port'] = os.environ.get(
     'FREEDOMBOX_SAMBA_PORT', config['DEFAULT']['samba_port'])
 
