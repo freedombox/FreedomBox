253e30c8e6fbd53e245ac8fcfbdc26b6c9daca46
Sunil Mohan Adapa <sunil@medhas.org>
2020-08-18 20:10:36 -0700

    bepasty: Add "Snippet" in category/short description
    Compared to other apps that do file sharing, sharing snippets that can be viewed
    in the browser is a unique distinction of bepasty.
    
    Signed-off-by: Sunil Mohan Adapa <sunil@medhas.org>
    Reviewed-by: Joseph Nuthalapati <njoseph@riseup.net>

--- plinth/modules/bepasty/__init__.py
+++ plinth/modules/bepasty/__init__.py
@@ -64,7 +64,7 @@ class BepastyApp(app_module.App):
 
         info = app_module.Info(self.app_id, version, name=_('bepasty'),
                                icon_filename='bepasty',
-                               short_description=_('File Sharing'),
+                               short_description=_('File & Snippet Sharing'),
                                description=_description, manual_page='bepasty',
                                clients=clients)
         self.add(info)
