956530ab8a10df0cb9f07ccf28b2987eae150352
Sunil Mohan Adapa <sunil@medhas.org>
2020-09-10 22:51:01 -0700

    mumble: Store and use a single domain for TLS certificate setup
    Certificate can be setup for a single domain at a time in Mumble. So, allow the
    user to choose the domain purely for this propose even though Mumble can work
    with multiple domains. Tell Let's Encrypt to work with this domain.
    
    Tests:
    
    - Without Mumble installed, change the domain name. Notice the mumble related
    certificate events are ignored.
    
    - Install Mumble, a TLS domain is automatically selected. Certificate is setup
    for that domain.
    
    - Ensure at least two domains are setup in the system. See the list in the
    Mumble app page. Choose a non-default domain. Domain should change and cert
    should be setup for that domain.
    
    - Go to config app and change the domain. Mumble domain should get set to a
    different domain and cert should get updated.
    
    - Install mumble without these changes. Apply the changes and start FreedomBox.
    Mumble app should get upgraded and certificate should get setup for a domain.
    
    Signed-off-by: Sunil Mohan Adapa <sunil@medhas.org>
    Reviewed-by: James Valleroy <jvalleroy@mailbox.org>

--- plinth/modules/mumble/forms.py
+++ plinth/modules/mumble/forms.py
@@ -6,9 +6,25 @@ Mumble server configuration form
 from django import forms
 from django.utils.translation import ugettext_lazy as _
 
+from plinth.modules import mumble
+
+
+def get_domain_choices():
+    """Double domain entries for inclusion in the choice field."""
+    return ((domain, domain) for domain in mumble.get_available_domains())
+
 
 class MumbleForm(forms.Form):
     """Mumble server configuration"""
+    domain = forms.ChoiceField(
+        choices=get_domain_choices,
+        label=_('TLS domain'),
+        help_text=_(
+            'Select a domain to use TLS with. If the list is empty, please '
+            'configure at least one domain with certificates.'),
+        required=False,
+    )
+
     super_user_password = forms.CharField(
         max_length=20,
         label=_('Set SuperUser Password'),
--- plinth/modules/mumble/views.py
+++ plinth/modules/mumble/views.py
@@ -3,6 +3,7 @@ from django.contrib import messages
 from django.utils.translation import ugettext_lazy as _
 
 from plinth import actions
+from plinth.modules import mumble
 from plinth.modules.mumble.forms import MumbleForm
 from plinth.views import AppView
 
@@ -11,9 +12,20 @@ class MumbleAppView(AppView):
     app_id = 'mumble'
     form_class = MumbleForm
 
+    def get_initial(self):
+        """Return the values to fill in the form."""
+        initial = super().get_initial()
+        initial['domain'] = mumble.get_domain()
+        return initial
+
     def form_valid(self, form):
         """Apply new superuser password if it exists"""
         new_config = form.cleaned_data
+
+        if mumble.get_domain() != new_config['domain']:
+            mumble.set_domain(new_config['domain'])
+            mumble.app.get_component('letsencrypt-mumble').setup_certificates()
+            messages.success(self.request, _('Configuration updated'))
 
         password = new_config.get('super_user_password')
         if password:
