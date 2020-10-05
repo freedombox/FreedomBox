e5cd5bd79658b2acfe3717729ec29a355f4d7f29
Sunil Mohan Adapa <sunil@medhas.org>
2020-10-04 21:00:13 -0700

    users: cosmetic: Yapf refactoring
    Signed-off-by: Sunil Mohan Adapa <sunil@medhas.org>

--- plinth/modules/users/forms.py
+++ plinth/modules/users/forms.py
@@ -66,8 +66,8 @@ class UsernameValidator(validators.RegexValidator):
 
 
 USERNAME_FIELD = forms.CharField(
-    label=ugettext_lazy('Username'),
-    max_length=150, validators=[UsernameValidator()],
+    label=ugettext_lazy('Username'), max_length=150,
+    validators=[UsernameValidator()],
     help_text=ugettext_lazy('Required. 150 characters or fewer. English '
                             'letters, digits and @/./-/_ only.'))
 
