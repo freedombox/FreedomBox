a5c8c90eb0ab9dc530f5d5ff6c5c151537e99009
Joseph Nuthalapati <njoseph@riseup.net>
2020-11-07 00:41:41 +0530

    gitlabci: Update Dockerfile and script
    Re-heating a server template is a bad idea in the case of Docker images
    since they are composed of immutable layers. The size of the image will
    keep going up with each subsequent build. Building using debian:testing
    instead.
    
    GitLab is no longer allowing docker login with the main password. A
    personal access token with the permissions of "api, read_registry,
    write_registry" should be used instead.
    
    Avoided copying the massive .container directory into the Docker
    container by using a .dockerignore file.
    
    Signed-off-by: Joseph Nuthalapati <njoseph@riseup.net>
    Reviewed-by: James Valleroy <jvalleroy@mailbox.org>

--- .ci/update-gitlabci-docker-image.sh
+++ .ci/update-gitlabci-docker-image.sh
@@ -1,9 +1,8 @@
 #! /bin/bash
 # SPDX-License-Identifier: AGPL-3.0-or-later
 
-# Provide your Salsa credentials here
 docker login registry.salsa.debian.org
 
-# This might need 20 minutes to run
-docker build -t registry.salsa.debian.org/freedombox-team/freedombox:gitlabci -f .ci/Dockerfile.gitlabci .
+# Build and upload a new image to the container registry
+DOCKER_BUILDKIT=1 docker build -t registry.salsa.debian.org/freedombox-team/freedombox:gitlabci -f .ci/Dockerfile.gitlabci .
 docker push registry.salsa.debian.org/freedombox-team/freedombox:gitlabci
