af1d21ce3b1b7daa5c7eb9755302b59a1ece743b
James Valleroy <jvalleroy@mailbox.org>
2021-07-04 20:37:32 -0400

    action_utils: Don't print when unholding freedombox package
    Some actions that use this function are expected to output
    JSON. Any output from apt-mark can interfere with this.
    
    Signed-off-by: James Valleroy <jvalleroy@mailbox.org>

--- plinth/action_utils.py
+++ plinth/action_utils.py
@@ -452,7 +452,8 @@ def apt_hold_freedombox():
 
 def apt_unhold_freedombox():
     """Remove any hold on freedombox package, and clear flag."""
-    subprocess.check_call(['apt-mark', 'unhold', 'freedombox'])
+    subprocess.run(['apt-mark', 'unhold', 'freedombox'],
+                   stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
     if apt_hold_flag.exists():
         apt_hold_flag.unlink()
 
