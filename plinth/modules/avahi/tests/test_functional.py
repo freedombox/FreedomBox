42c2bcfde74ae7d20e41d2693745e21a004a11c7
James Valleroy <jvalleroy@mailbox.org>
2021-09-06 18:04:38 -0400

    avahi: Convert functional tests to non-BDD python format
    Signed-off-by: James Valleroy <jvalleroy@mailbox.org>
    [sunil: Add markers]
    Signed-off-by: Sunil Mohan Adapa <sunil@medhas.org>
    Reviewed-by: Sunil Mohan Adapa <sunil@medhas.org>

--- plinth/modules/avahi/tests/test_functional.py
+++ plinth/modules/avahi/tests/test_functional.py
@@ -3,6 +3,27 @@
 Functional, browser based tests for avahi app.
 """
 
-from pytest_bdd import scenarios
+import pytest
+from plinth.tests import functional
 
-scenarios('avahi.feature')
+pytestmark = [pytest.mark.system, pytest.mark.essential, pytest.mark.avahi]
+
+
+@pytest.fixture(scope='module', autouse=True)
+def fixture_background(session_browser):
+    """Login and install the app."""
+    functional.login(session_browser)
+    functional.install(session_browser, 'avahi')
+    yield
+    functional.app_disable(session_browser, 'avahi')
+
+
+def test_enable_disable(session_browser):
+    """Test enabling the app."""
+    functional.app_disable(session_browser, 'avahi')
+
+    functional.app_enable(session_browser, 'avahi')
+    assert functional.service_is_running(session_browser, 'avahi')
+
+    functional.app_disable(session_browser, 'avahi')
+    assert functional.service_is_not_running(session_browser, 'avahi')
