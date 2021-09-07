9cc69ed32c7d2bbcda4cf55d802c631b26b9b544
James Valleroy <jvalleroy@mailbox.org>
2021-09-07 11:01:46 -0400

    matrixsynapse: Convert functional tests to non-BDD python format
    Signed-off-by: James Valleroy <jvalleroy@mailbox.org>
    [sunil: Add markers]
    Signed-off-by: Sunil Mohan Adapa <sunil@medhas.org>
    Reviewed-by: Sunil Mohan Adapa <sunil@medhas.org>

--- plinth/modules/matrixsynapse/tests/test_functional.py
+++ plinth/modules/matrixsynapse/tests/test_functional.py
@@ -3,6 +3,30 @@
 Functional, browser based tests for matrixsynapse app.
 """
 
-from pytest_bdd import scenarios
+import pytest
+from plinth.tests import functional
 
-scenarios('matrixsynapse.feature')
+pytestmark = [pytest.mark.apps, pytest.mark.matrixsynapse]
+
+
+@pytest.fixture(scope='module', autouse=True)
+def fixture_background(session_browser):
+    """Login and install the app."""
+    functional.login(session_browser)
+    functional.set_domain_name(session_browser, 'mydomain.example')
+    functional.install(session_browser, 'matrixsynapse')
+    functional.app_select_domain_name(session_browser, 'matrixsynapse',
+                                      'mydomain.example')
+    yield
+    functional.app_disable(session_browser, 'matrixsynapse')
+
+
+def test_enable_disable(session_browser):
+    """Test enabling the app."""
+    functional.app_disable(session_browser, 'matrixsynapse')
+
+    functional.app_enable(session_browser, 'matrixsynapse')
+    assert functional.service_is_running(session_browser, 'matrixsynapse')
+
+    functional.app_disable(session_browser, 'matrixsynapse')
+    assert functional.service_is_not_running(session_browser, 'matrixsynapse')
