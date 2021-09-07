fa6aed9c7a6412bdfd11d717cae6256963005bc7
James Valleroy <jvalleroy@mailbox.org>
2021-09-06 20:02:21 -0400

    minetest: Convert functional tests to non-BDD python format
    Signed-off-by: James Valleroy <jvalleroy@mailbox.org>
    [sunil: Add markers]
    Signed-off-by: Sunil Mohan Adapa <sunil@medhas.org>
    Reviewed-by: Sunil Mohan Adapa <sunil@medhas.org>

--- plinth/modules/minetest/tests/test_functional.py
+++ plinth/modules/minetest/tests/test_functional.py
@@ -3,6 +3,27 @@
 Functional, browser based tests for minetest app.
 """
 
-from pytest_bdd import scenarios
+import pytest
+from plinth.tests import functional
 
-scenarios('minetest.feature')
+pytestmark = [pytest.mark.apps, pytest.mark.minetest]
+
+
+@pytest.fixture(scope='module', autouse=True)
+def fixture_background(session_browser):
+    """Login and install the app."""
+    functional.login(session_browser)
+    functional.install(session_browser, 'minetest')
+    yield
+    functional.app_disable(session_browser, 'minetest')
+
+
+def test_enable_disable(session_browser):
+    """Test enabling the app."""
+    functional.app_disable(session_browser, 'minetest')
+
+    functional.app_enable(session_browser, 'minetest')
+    assert functional.service_is_running(session_browser, 'minetest')
+
+    functional.app_disable(session_browser, 'minetest')
+    assert functional.service_is_not_running(session_browser, 'minetest')
