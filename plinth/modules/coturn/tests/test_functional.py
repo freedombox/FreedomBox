397d6a424e6240bef97679166eafcb86d663ca0a
James Valleroy <jvalleroy@mailbox.org>
2021-09-20 20:04:37 -0400

    coturn: Convert functional tests to non-BDD python format
    Signed-off-by: James Valleroy <jvalleroy@mailbox.org>
    Reviewed-by: Sunil Mohan Adapa <sunil@medhas.org>

--- plinth/modules/coturn/tests/test_functional.py
+++ plinth/modules/coturn/tests/test_functional.py
@@ -3,6 +3,36 @@
 Functional, browser based tests for coturn app.
 """
 
-from pytest_bdd import scenarios
+import pytest
+from plinth.tests import functional
 
-scenarios('coturn.feature')
+pytestmark = [pytest.mark.apps, pytest.mark.coturn]
+
+
+@pytest.fixture(scope='module', autouse=True)
+def fixture_background(session_browser):
+    """Login and install the app."""
+    functional.login(session_browser)
+    functional.install(session_browser, 'coturn')
+    yield
+    functional.app_disable(session_browser, 'coturn')
+
+
+def test_enable_disable(session_browser):
+    """Test enabling the app."""
+    functional.app_disable(session_browser, 'coturn')
+
+    functional.app_enable(session_browser, 'coturn')
+    assert functional.service_is_running(session_browser, 'coturn')
+
+    functional.app_disable(session_browser, 'coturn')
+    assert functional.service_is_not_running(session_browser, 'coturn')
+
+
+@pytest.mark.backups
+def test_backup(session_browser):
+    # TODO: Improve this by checking that secret and domain did not change
+    functional.app_enable(session_browser, 'coturn')
+    functional.backup_create(session_browser, 'coturn', 'test_coturn')
+    functional.backup_restore(session_browser, 'coturn', 'test_coturn')
+    assert functional.service_is_running(session_browser, 'coturn')
