757d24c2bede9dc9b08035287e3be9ca2264e494
Sunil Mohan Adapa <sunil@medhas.org>
2021-09-22 14:40:32 -0700

    ejabberd: tests: Use common fixtures for testing actions module
    Signed-off-by: Sunil Mohan Adapa <sunil@medhas.org>
    Reviewed-by: Veiko Aasa <veiko17@disroot.org>

--- plinth/modules/ejabberd/tests/test_turn_config.py
+++ plinth/modules/ejabberd/tests/test_turn_config.py
@@ -3,7 +3,11 @@
 Test module for ejabberd STUN/TURN configuration.
 """
 
-from unittest.mock import patch
+import pathlib
+import shutil
+from unittest.mock import MagicMock, patch
+
+import pytest
 
 from plinth.modules import ejabberd
 from plinth.modules.coturn.components import TurnConfiguration
@@ -15,6 +19,50 @@ managed_configuration = TurnConfiguration(
 overridden_configuration = TurnConfiguration(
     'public.coturn.site', [],
     'BUeKvKzhAsTZ8MEwMd3yTwpr2uvbOxgWe51aiP02OAGyOlj6WGuCyqj7iaOsbkC7')
+
+actions_name = 'ejabberd'
+
+current_directory = pathlib.Path(__file__).parent
+
+
+@pytest.fixture(name='conf_file')
+def fixture_conf_file(tmp_path):
+    """Uses a dummy configuration file."""
+    settings_file_name = 'ejabberd.yml'
+    conf_file = tmp_path / settings_file_name
+    conf_file.touch()
+    shutil.copyfile(str(current_directory / 'data' / 'ejabberd.yml.example'),
+                    str(conf_file))
+    return str(conf_file)
+
+
+@pytest.fixture(name='managed_file')
+def fixture_managed_file(tmp_path):
+    """Uses a dummy managed file."""
+    file_name = 'freedombox_managed_coturn'
+    fil = tmp_path / file_name
+    return str(fil)
+
+
+@pytest.fixture(autouse=True)
+def fixture_set_configuration_paths(actions_module, conf_file, managed_file):
+    """Setup configuration file paths in action module."""
+    actions_module.EJABBERD_CONFIG = conf_file
+    actions_module.EJABBERD_MANAGED_COTURN = managed_file
+
+
+@pytest.fixture(name='test_configuration', autouse=True)
+def fixture_test_configuration(call_action, conf_file):
+    """Use a separate ejabberd configuration for tests.
+
+    Patches actions.superuser_run with the fixture call_action.
+    The module state is patched to be 'up-to-date'.
+    """
+    with patch('plinth.actions.superuser_run', call_action):
+        helper = MagicMock()
+        helper.get_state.return_value = 'up-to-date'
+        ejabberd.setup_helper = helper
+        yield
 
 
 def _set_turn_configuration(monkeypatch, config=managed_configuration,
