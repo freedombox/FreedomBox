a89b4de8b7508bee8ef61e20c2b72ed4739d109c
James Valleroy <jvalleroy@mailbox.org>
2021-10-04 21:36:30 -0400

    zoph: Convert functional tests to non-BDD python format
    Signed-off-by: James Valleroy <jvalleroy@mailbox.org>
    Reviewed-by: Sunil Mohan Adapa <sunil@medhas.org>

--- plinth/modules/zoph/tests/test_functional.py
+++ plinth/modules/zoph/tests/test_functional.py
@@ -3,14 +3,42 @@
 Functional, browser based tests for zoph app.
 """
 
-from pytest_bdd import given, parsers, scenarios
-
+import pytest
 from plinth.tests import functional
 
-scenarios('zoph.feature')
+pytestmark = [pytest.mark.apps, pytest.mark.zoph]
 
 
-@given(parsers.parse('the zoph application is setup'))
+@pytest.fixture(scope='module', autouse=True)
+def fixture_background(session_browser):
+    """Login and install the app."""
+    functional.login(session_browser)
+    functional.install(session_browser, 'zoph')
+    _zoph_is_setup(session_browser)
+    yield
+    functional.app_disable(session_browser, 'zoph')
+
+
+def test_enable_disable(session_browser):
+    """Test enabling the app."""
+    functional.app_disable(session_browser, 'zoph')
+
+    functional.app_enable(session_browser, 'zoph')
+    assert functional.app_is_enabled(session_browser, 'zoph')
+
+    functional.app_disable(session_browser, 'zoph')
+    assert not functional.app_is_enabled(session_browser, 'zoph')
+
+
+@pytest.mark.backups
+def test_backup_restore(session_browser):
+    """Test backup and restore."""
+    functional.app_enable(session_browser, 'zoph')
+    functional.backup_create(session_browser, 'zoph', 'test_zoph')
+    functional.backup_restore(session_browser, 'zoph', 'test_zoph')
+    assert functional.app_is_enabled(session_browser, 'zoph')
+
+
 def _zoph_is_setup(session_browser):
     """Click setup button on the setup page."""
     functional.nav_to_module(session_browser, 'zoph')
