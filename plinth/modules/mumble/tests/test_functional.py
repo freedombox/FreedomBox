ed46278550ebf31c1efb4576c246aa89d1a6e99d
James Valleroy <jvalleroy@mailbox.org>
2021-10-16 09:12:13 -0400

    mumble: Use BaseAppTests for functional tests
    Signed-off-by: James Valleroy <jvalleroy@mailbox.org>
    Reviewed-by: Sunil Mohan Adapa <sunil@medhas.org>

--- plinth/modules/mumble/tests/test_functional.py
+++ plinth/modules/mumble/tests/test_functional.py
@@ -4,36 +4,19 @@ Functional, browser based tests for mumble app.
 """
 
 import pytest
-from plinth.tests import functional
+
+from plinth.tests.functional import BaseAppTests
 
 pytestmark = [pytest.mark.apps, pytest.mark.mumble]
 
 
-@pytest.fixture(scope='module', autouse=True)
-def fixture_background(session_browser):
-    """Login and install the app."""
-    functional.login(session_browser)
-    functional.install(session_browser, 'mumble')
-    yield
-    functional.app_disable(session_browser, 'mumble')
+class TestMumbleApp(BaseAppTests):
+    app_name = 'mumble'
+    has_service = True
+    has_web = False
 
+    # TODO: Requires a valid domain with certificates to complete setup.
+    check_diagnostics = False
 
-def test_enable_disable(session_browser):
-    """Test enabling the app."""
-    functional.app_disable(session_browser, 'mumble')
-
-    functional.app_enable(session_browser, 'mumble')
-    assert functional.service_is_running(session_browser, 'mumble')
-
-    functional.app_disable(session_browser, 'mumble')
-    assert functional.service_is_not_running(session_browser, 'mumble')
-
-
-# TODO: Improve this to actually check that data such as rooms, identity or
-# certificates are restored.
-def test_backup_restore(session_browser):
-    """Test backup and restore."""
-    functional.app_enable(session_browser, 'mumble')
-    functional.backup_create(session_browser, 'mumble', 'test_mumble')
-    functional.backup_restore(session_browser, 'mumble', 'test_mumble')
-    assert functional.service_is_running(session_browser, 'mumble')
+    # TODO: Improve test_backup_restore to actually check that data such
+    # as rooms, identity or certificates are restored.
