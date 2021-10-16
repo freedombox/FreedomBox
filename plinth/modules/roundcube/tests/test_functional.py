6358d4fcd346471ee028acaaf214396ca0c503cf
James Valleroy <jvalleroy@mailbox.org>
2021-10-16 09:18:42 -0400

    roundcube: Use BaseAppTests for functional tests
    Signed-off-by: James Valleroy <jvalleroy@mailbox.org>
    Reviewed-by: Sunil Mohan Adapa <sunil@medhas.org>

--- plinth/modules/roundcube/tests/test_functional.py
+++ plinth/modules/roundcube/tests/test_functional.py
@@ -4,35 +4,13 @@ Functional, browser based tests for roundcube app.
 """
 
 import pytest
-from plinth.tests import functional
+
+from plinth.tests.functional import BaseAppTests
 
 pytestmark = [pytest.mark.apps, pytest.mark.roundcube]
 
 
-@pytest.fixture(scope='module', autouse=True)
-def fixture_background(session_browser):
-    """Login and install the app."""
-    functional.login(session_browser)
-    functional.install(session_browser, 'roundcube')
-    yield
-    functional.app_disable(session_browser, 'roundcube')
-
-
-def test_enable_disable(session_browser):
-    """Test enabling the app."""
-    functional.app_disable(session_browser, 'roundcube')
-
-    functional.app_enable(session_browser, 'roundcube')
-    assert functional.is_available(session_browser, 'roundcube')
-
-    functional.app_disable(session_browser, 'roundcube')
-    assert not functional.is_available(session_browser, 'roundcube')
-
-
-@pytest.mark.backups
-def test_backup_restore(session_browser):
-    """Test backup and restore."""
-    functional.app_enable(session_browser, 'roundcube')
-    functional.backup_create(session_browser, 'roundcube', 'test_roundcube')
-    functional.backup_restore(session_browser, 'roundcube', 'test_roundcube')
-    assert functional.is_available(session_browser, 'roundcube')
+class TestRoundcubeApp(BaseAppTests):
+    app_name = 'roundcube'
+    has_service = False
+    has_web = True
