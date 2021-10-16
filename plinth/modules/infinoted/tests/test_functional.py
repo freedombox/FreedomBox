fae88d9fff1b7b0d553fdba3160c25fd66b1cbb1
James Valleroy <jvalleroy@mailbox.org>
2021-10-16 07:57:11 -0400

    infinoted: Use BaseAppTests for functional tests
    Signed-off-by: James Valleroy <jvalleroy@mailbox.org>
    Reviewed-by: Sunil Mohan Adapa <sunil@medhas.org>

--- plinth/modules/infinoted/tests/test_functional.py
+++ plinth/modules/infinoted/tests/test_functional.py
@@ -5,26 +5,12 @@ Functional, browser based tests for infinoted app.
 
 import pytest
 
-from plinth.tests import functional
+from plinth.tests.functional import BaseAppTests
 
 pytestmark = [pytest.mark.apps, pytest.mark.infinoted]
 
 
-@pytest.fixture(scope='module', autouse=True)
-def fixture_background(session_browser):
-    """Login and install the app."""
-    functional.login(session_browser)
-    functional.install(session_browser, 'infinoted')
-    yield
-    functional.app_disable(session_browser, 'infinoted')
-
-
-def test_enable_disable(session_browser):
-    """Test enabling the app."""
-    functional.app_disable(session_browser, 'infinoted')
-
-    functional.app_enable(session_browser, 'infinoted')
-    assert functional.service_is_running(session_browser, 'infinoted')
-
-    functional.app_disable(session_browser, 'infinoted')
-    assert functional.service_is_not_running(session_browser, 'infinoted')
+class TestInfinotedApp(BaseAppTests):
+    app_name = 'infinoted'
+    has_service = True
+    has_web = False
