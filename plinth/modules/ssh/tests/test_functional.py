017b1b43dacfd7e72c71815a34196848ca562960
James Valleroy <jvalleroy@mailbox.org>
2021-10-21 19:36:01 -0400

    ssh: Use BaseAppTests for functional tests
    Signed-off-by: James Valleroy <jvalleroy@mailbox.org>
    Reviewed-by: Sunil Mohan Adapa <sunil@medhas.org>

--- plinth/modules/ssh/tests/test_functional.py
+++ plinth/modules/ssh/tests/test_functional.py
@@ -5,37 +5,15 @@ Functional, browser based tests for ssh app.
 
 import pytest
 
-from plinth.tests import functional
+from plinth.tests.functional import BaseAppTests
 
 pytestmark = [pytest.mark.system, pytest.mark.ssh]
 
 
-@pytest.fixture(scope='module', autouse=True)
-def fixture_background(session_browser):
-    """Login and install the app."""
-    functional.login(session_browser)
-    functional.install(session_browser, 'ssh')
-    yield
-    functional.app_enable(session_browser, 'ssh')
+class TestSshApp(BaseAppTests):
+    app_name = 'ssh'
+    has_service = True
+    has_web = False
 
-
-def test_enable_disable(session_browser):
-    """Test enabling the app."""
-    functional.app_disable(session_browser, 'ssh')
-
-    functional.app_enable(session_browser, 'ssh')
-    assert functional.service_is_running(session_browser, 'ssh')
-
-    functional.app_disable(session_browser, 'ssh')
-    assert functional.service_is_not_running(session_browser, 'ssh')
-
-
-# TODO: Improve this to actually check that earlier ssh certificate has been
-# restored.
-@pytest.mark.backups
-def test_backup_restore(session_browser):
-    """Test backup and restore."""
-    functional.app_enable(session_browser, 'ssh')
-    functional.backup_create(session_browser, 'ssh', 'test_ssh')
-    functional.backup_restore(session_browser, 'ssh', 'test_ssh')
-    assert functional.service_is_running(session_browser, 'ssh')
+    # TODO: Improve test_backup_restore to actually check that earlier
+    # ssh certificate has been restored.
