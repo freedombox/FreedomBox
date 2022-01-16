44bff730f3d8272c3f4707dc98fbe2320675e480
Benedek Nagy <nagybenedek@kumbatmo.hu>
2022-01-16 19:14:43 +0000

    shaarli: Add android app to description
    [sunil: Fix typo, indentation and add module comment]
    Signed-off-by: Sunil Mohan Adapa <sunil@medhas.org>
    Reviewed-by: Sunil Mohan Adapa <sunil@medhas.org>

--- plinth/modules/shaarli/manifest.py
+++ plinth/modules/shaarli/manifest.py
@@ -1,11 +1,30 @@
 # SPDX-License-Identifier: AGPL-3.0-or-later
+"""
+Application manifest for bind.
+"""
 
 from django.utils.translation import gettext_lazy as _
 
+from plinth.clients import store_url
+
 clients = [{
+    'name':
+        _('Shaarlier'),
+    'platforms': [{
+        'type': 'store',
+        'os': 'android',
+        'store_name': 'google-play',
+        'url': store_url('google-play', 'com.dimtion.shaarlier'),
+    }, {
+        'type': 'store',
+        'os': 'android',
+        'store_name': 'f-droid',
+        'url': store_url('f-droid', 'com.dimtion.shaarlier'),
+    }]
+}, {
     'name': _('Shaarli'),
     'platforms': [{
         'type': 'web',
-        'url': '/shaarli'
+        'url': '/shaarli/'
     }]
 }]
