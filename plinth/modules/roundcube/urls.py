affffddf362fa274b7fa151344e949f5a44f36ea
Sunil Mohan Adapa <sunil@medhas.org>
2022-01-26 16:39:08 -0800

    roundcube: Add setting for local connection only
    When this option is enabled, it would make the interface easy to work with. This
    is likely what most users would want. Don't break things for users who have
    already installed roundcube and ensure that local only is disable for them.
    
    Tests:
    
    - Install roundcube without the patch. Disable the app. Apply patch. Restart
    service. Notice that roundcube is not re-enabled.
    
    - Install roundcube without the patch. Apply patch. Restart service. Notice that
    roundcube configuration /etc/roundcube/config.inc.php file has been updated and
    include_once() at the end has been added. The file
    /etc/roundcube/freedombox-config.php has been added. Local only option is
    disabled.
    
    - Install roundcube freshly with the patch. Local only option is enabled. Open
    interface. Notice that server option is not presented.
    
    - Disable local only option and notice that server field is shown in the
    interface.
    
    Signed-off-by: Sunil Mohan Adapa <sunil@medhas.org>
    [jvalleroy: Fix comment]
    Signed-off-by: James Valleroy <jvalleroy@mailbox.org>
    Reviewed-by: James Valleroy <jvalleroy@mailbox.org>

--- plinth/modules/roundcube/urls.py
+++ plinth/modules/roundcube/urls.py
@@ -5,9 +5,8 @@ URLs for the Roundcube module.
 
 from django.urls import re_path
 
-from plinth.views import AppView
+from .views import RoundcubeAppView
 
 urlpatterns = [
-    re_path(r'^apps/roundcube/$', AppView.as_view(app_id='roundcube'),
-            name='index')
+    re_path(r'^apps/roundcube/$', RoundcubeAppView.as_view(), name='index')
 ]
