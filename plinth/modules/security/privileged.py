253540fb3d12254c920b632cc484be6a79d27229
Sunil Mohan Adapa <sunil@medhas.org>
2022-11-11 11:52:11 -0800

    security: Remove restricted access setting and configuration
    Closes: #2276.
    
    Functionality all over the system keeps failing due this approach. The latest is
    changing hostname in ejabberd Mnesia database fails (#2276). Further, users
    connecting FreedomBox to a monitor can't use a GUI.
    
    Tests:
    
    - Without patches, enable restricted access. Apply patches and setup.py install.
    Security app is updated. Restricted access is disabled and
    /etc/security/access.d/{50freedombox.conf, 10freedombox-security.conf,
    10freedombox-performance.conf} are removed. It is possible to login into
    non-admin account via SSH.
    
    - On a fresh install, the configuration files are not found.
    
    - Security page does not show 'restrict console logins' option.
    
    - Updating security app setting works. Message 'Configuration updated.' is
    shown.
    
    - First boot succeeds. Restrict console login is not enabled.
    
    Signed-off-by: Sunil Mohan Adapa <sunil@medhas.org>
    Reviewed-by: James Valleroy <jvalleroy@mailbox.org>

--- plinth/modules/security/privileged.py
+++ plinth/modules/security/privileged.py
@@ -13,18 +13,6 @@ ACCESS_CONF_SNIPPETS = [OLD_ACCESS_CONF_SNIPPET, ACCESS_CONF_SNIPPET]
 
 
 @privileged
-def enable_restricted_access():
-    """Restrict console login to users in admin or sudo group."""
-    try:
-        os.mkdir(os.path.dirname(ACCESS_CONF_FILE))
-    except FileExistsError:
-        pass
-
-    with open(ACCESS_CONF_FILE, 'w', encoding='utf-8') as conffile:
-        conffile.write(ACCESS_CONF_SNIPPET + '\n')
-
-
-@privileged
 def disable_restricted_access():
     """Don't restrict console login to users in admin or sudo group."""
     with open(ACCESS_CONF_FILE_OLD, 'r', encoding='utf-8') as conffile:
@@ -39,18 +27,3 @@ def disable_restricted_access():
         os.remove(ACCESS_CONF_FILE)
     except OSError:
         pass
-
-
-def get_restricted_access_enabled():
-    """Return whether restricted access is enabled."""
-    with open(ACCESS_CONF_FILE_OLD, 'r', encoding='utf-8') as conffile:
-        if any(line.strip() in ACCESS_CONF_SNIPPETS
-               for line in conffile.readlines()):
-            return True
-
-    try:
-        with open(ACCESS_CONF_FILE, 'r', encoding='utf-8') as conffile:
-            return any(line.strip() in ACCESS_CONF_SNIPPETS
-                       for line in conffile.readlines())
-    except FileNotFoundError:
-        return False
