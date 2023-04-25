537cc749d1e6788dc59dcccc3b2763e7ce710c8f
Sunil Mohan Adapa <sunil@medhas.org>
2023-04-25 11:34:18 -0700

    ttrss: Update list of clients
    - Tiny Tiny RSS Fork calls itself "Tiny Tiny RSS" on F-Droid and "TTTRSS" on
    project page.
    
    - TTRSS-Reader is available on F-Droid and Google Play Store.
    
    - Geekttrss is available on F-Droid.
    
    - The original TTRSS app does not seem to be available on Google Play Store
    anymore.
    
    Tests:
    
    - Install each app on respective platforms. Configure them and see if they work.
    
    - Check for free software licenses on project pages.
    
    - Click on each of the clients in the clients list in the web UI and see that
    the links redirect to corresponding app pages on the appropriate store.
    
    Signed-off-by: Sunil Mohan Adapa <sunil@medhas.org>
    Reviewed-by: James Valleroy <jvalleroy@mailbox.org>

--- plinth/modules/ttrss/manifest.py
+++ plinth/modules/ttrss/manifest.py
@@ -4,28 +4,50 @@ from django.utils.translation import gettext_lazy as _
 
 from plinth.clients import store_url
 
-clients = [{
-    'name':
-        _('Tiny Tiny RSS (Fork)'),
-    'platforms': [{
-        'type': 'store',
-        'os': 'android',
-        'store_name': 'f-droid',
-        'url': store_url('f-droid', 'org.fox.tttrss')
-    }]
-}, {
-    'name':
-        _('Tiny Tiny RSS'),
-    'platforms': [{
-        'type': 'store',
-        'os': 'android',
-        'store_name': 'google-play',
-        'url': store_url('google-play', 'org.fox.ttrss')
-    }, {
-        'type': 'web',
-        'url': '/tt-rss'
-    }]
-}]
+clients = [
+    {
+        'name':
+            _('Tiny Tiny RSS (TTTRSS)'),
+        'platforms': [{
+            'type': 'store',
+            'os': 'android',
+            'store_name': 'f-droid',
+            'url': store_url('f-droid', 'org.fox.tttrss')
+        }]
+    },
+    {
+        'name':
+            _('TTRSS-Reader'),
+        'platforms': [{
+            'type': 'store',
+            'os': 'android',
+            'store_name': 'google-play',
+            'url': store_url('google-play', 'org.ttrssreader')
+        }, {
+            'type': 'store',
+            'os': 'android',
+            'store_name': 'f-droid',
+            'url': store_url('f-droid', 'org.ttrssreader')
+        }]
+    },
+    {
+        'name':
+            _('Geekttrss'),
+        'platforms': [{
+            'type': 'store',
+            'os': 'android',
+            'store_name': 'google-play',
+            'url': store_url('google-play', 'com.geekorum.ttrss')
+        }]
+    },
+    {
+        'name': _('Tiny Tiny RSS'),
+        'platforms': [{
+            'type': 'web',
+            'url': '/tt-rss'
+        }]
+    },
+]
 
 backup = {
     'data': {
