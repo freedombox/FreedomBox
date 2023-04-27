31e46164706739a5797ef8dc783d95425adf2a52
Sunil Mohan Adapa <sunil@medhas.org>
2023-04-26 18:12:10 -0700

    transmission: Add Tremotesf to list of client apps
    Test:
    
    - From F-droid, install and configure the app. Ensure that it works with
    FreedomBox.
    
    Signed-off-by: Sunil Mohan Adapa <sunil@medhas.org>
    Reviewed-by: James Valleroy <jvalleroy@mailbox.org>

--- plinth/modules/transmission/manifest.py
+++ plinth/modules/transmission/manifest.py
@@ -2,11 +2,27 @@
 
 from django.utils.translation import gettext_lazy as _
 
+from plinth.clients import store_url
+
 clients = [{
     'name': _('Transmission'),
     'platforms': [{
         'type': 'web',
         'url': '/transmission'
+    }]
+}, {
+    'name':
+        _('Tremotesf'),
+    'platforms': [{
+        'type': 'store',
+        'os': 'android',
+        'store_name': 'f-droid',
+        'url': store_url('f-droid', 'org.equeim.tremotesf')
+    }, {
+        'type': 'store',
+        'os': 'android',
+        'store_name': 'google-play',
+        'url': store_url('google-play', 'org.equeim.tremotesf')
     }]
 }]
 
