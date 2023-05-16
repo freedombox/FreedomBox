3d299a7b8ad64e490a9d8cc3c787b7f8ebde5a66
Sunil Mohan Adapa <sunil@medhas.org>
2023-05-16 13:43:39 -0700

    syncthing: Use drop-in config component for /etc files
    Tests:
    
    - Config files are all symlinks in /etc/
    - Web interface works
    
    Signed-off-by: Sunil Mohan Adapa <sunil@medhas.org>
    Reviewed-by: James Valleroy <jvalleroy@mailbox.org>

--- plinth/modules/syncthing/__init__.py
+++ plinth/modules/syncthing/__init__.py
@@ -5,6 +5,7 @@ from django.utils.translation import gettext_lazy as _
 
 from plinth import app as app_module
 from plinth import cfg, frontpage, menu
+from plinth.config import DropinConfigs
 from plinth.daemon import Daemon
 from plinth.modules.apache.components import Webserver
 from plinth.modules.backups.components import BackupRestore
@@ -42,7 +43,7 @@ class SyncthingApp(app_module.App):
 
     app_id = 'syncthing'
 
-    _version = 6
+    _version = 7
 
     DAEMON = 'syncthing@syncthing'
 
@@ -78,6 +79,11 @@ class SyncthingApp(app_module.App):
 
         packages = Packages('packages-syncthing', ['syncthing'])
         self.add(packages)
+
+        dropin_configs = DropinConfigs('dropin-configs-syncthing', [
+            '/etc/apache2/conf-available/syncthing-plinth.conf',
+        ])
+        self.add(dropin_configs)
 
         firewall = Firewall('firewall-syncthing-web', info.name,
                             ports=['http', 'https'], is_external=True)
