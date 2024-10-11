3742ab8f6b52d57685e9febc9ba792e02702f10c
Sunil Mohan Adapa <sunil@medhas.org>
2024-10-11 08:12:34 -0700

    ci: Generalize script to update container, switch to podman
    Signed-off-by: Sunil Mohan Adapa <sunil@medhas.org>
    Reviewed-by: Veiko Aasa <veiko17@disroot.org>

---
+++ .ci/update-container-image.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+# SPDX-License-Identifier: AGPL-3.0-or-later
+
+CONTAINER=$1
+CONTAINER="${CONTAINER:-gitlabci}"
+
+podman login registry.salsa.debian.org
+
+# Build and upload a new image to the container registry
+podman build -t registry.salsa.debian.org/freedombox-team/freedombox:${CONTAINER} -f .ci/Dockerfile.${CONTAINER} .
+podman push registry.salsa.debian.org/freedombox-team/freedombox:${CONTAINER}
--- .ci/update-gitlabci-docker-image.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-# SPDX-License-Identifier: AGPL-3.0-or-later
-
-docker login registry.salsa.debian.org
-
-# Build and upload a new image to the container registry
-DOCKER_BUILDKIT=1 docker build -t registry.salsa.debian.org/freedombox-team/freedombox:gitlabci -f .ci/Dockerfile.gitlabci .
-docker push registry.salsa.debian.org/freedombox-team/freedombox:gitlabci
