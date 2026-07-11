10069c4e5d353401533420e05d77bb6a8460b442
James Valleroy <jvalleroy@mailbox.org>
2026-07-11 15:39:06 -0400

    tor, torproxy: Avoid error if defaults file missing during uninstall
    See:
    https://discuss.freedombox.org/t/error-when-uninstalling-tor-and-tor-proxy/4295
    
    sunil: Use pathlib.Path.unlink()
    
    Tests:
    
    - Install Tor and Tor Proxy. Uninstall both. Uninstall is succesful.
    
    - Install Tor and Tor Proxy. Manually delete
      /var/run/tor-instances/*.defaults. Uninstall both apps. Uninstall is
      successful. A warning is printed in the logs.
    
    Signed-off-by: James Valleroy <jvalleroy@mailbox.org>
    Signed-off-by: Sunil Mohan Adapa <sunil@medhas.org>
    Reviewed-by: Sunil Mohan Adapa <sunil@medhas.org>

--- plinth/modules/tor/privileged.py
+++ plinth/modules/tor/privileged.py
@@ -494,5 +494,7 @@ def uninstall():
     for directory in directories:
         shutil.rmtree(directory, ignore_errors=True)
 
-    os.unlink(f'/var/run/tor-instances/{INSTANCE_NAME}.defaults')
+    defaults_file = f'/var/run/tor-instances/{INSTANCE_NAME}.defaults'
+    pathlib.Path(defaults_file).unlink(missing_ok=True)
+
     action_utils.service_unmask(SERVICE_NAME)
